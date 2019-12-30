732d6ec5e2c8c84732dad299e54178f6c5b9c8e2
zombie110year <zombie110year@outlook.com>
2019-12-30 22:02:35 +0800

    nikola base config

---
+++ conf.py
@@ -0,0 +1,10 @@
+###############################################################################
+# Nikola Configurations
+###############################################################################
+BLOG_AUTHOR = "zombie110year"
+BLOG_TITLE = "Mo 默"
+SITE_URL = "https://blog.zombie110year.top/"
+BLOG_EMAIL = "zombie110year@outlook.com"
+BLOG_DESCRIPTION = "反正没人看，就随便写写咯"
+
+
